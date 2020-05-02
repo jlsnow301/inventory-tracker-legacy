f770fef7c93826fa5febe4232ac9c446716eec46
jlsnow301 <jlsnow.301@gmail.com>
2020-05-02 04:03:25 -0700

    jlsnow301

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -1,41 +1,75 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import PopupButton from "./PopupButton";
+import styled from "@emotion/styled";
 
-const toolbarStyle = {
-  display: "flex",
-  alignself: "stretch",
-  backgroundColor: "#FE2E2E",
-  padding: "6px 8px",
-  height: "75px",
+const Toolbar = ({ ukey }) => {
+  // Styles
+  const toolbarStyle = {
+    display: "flex",
+    alignself: "stretch",
+    backgroundColor: "#FE2E2E",
+    padding: "6px 8px",
+    height: "75px",
+  };
+  const headerStyle = {
+    color: "white",
+    alignself: "stretch",
+    fontSize: "40px",
+    paddingLeft: "30px",
+    margin: "auto",
+    fontFamily: "Fira Sans",
+  };
+
+  const Buttons = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  `;
+
+  // Handles buttons
+  const loggedIn = ukey == null;
+  const [query, setQuery] = useState("");
+
+  // Get the search, if logged in
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getItem(query);
+  };
+
+  // Database call
+  const getItem = (q, ukey) => {};
+
+  // Returns the toolbar
+  return (
+    <div style={toolbarStyle}>
+      <img
+        className="icon"
+        src="this_is_fine.png"
+        alt="Logo"
+        height="70"
+        width="70"
+      />
+      <p style={headerStyle}>Slogan</p>
+      <div style={{ flex: 1 }}></div>
+      {!loggedIn ? (
+        <Buttons>
+          <PopupButton button="login" />
+          <PopupButton button="about" />
+          <PopupButton button="contact" />
+        </Buttons>
+      ) : (
+        <div>
+          <Buttons>
+            <PopupButton button="advsearch" />
+            <PopupButton button="overview" />
+            <PopupButton button="settings" />
+          </Buttons>
+          <input value={query} onChange={(e) => setQuery(e.target.value)} />
+          <button onClick={(e) => handleSearch(e)}>Search</button>
+        </div>
+      )}
+    </div>
+  );
 };
-
-const headerStyle = {
-  color: "white",
-  alignself: "stretch",
-  fontSize: "40px",
-  paddingLeft: "30px",
-  margin: "auto",
-  fontFamily: "Fira Sans",
-};
-
-class Toolbar extends Component {
-  render() {
-    return (
-      <div style={toolbarStyle}>
-        <img
-          className="icon"
-          src="this_is_fine.png"
-          alt="Logo"
-          height="70"
-          width="70"
-        />
-        <p style={headerStyle}>Slogan</p>
-        <div style={{ flex: 1 }}></div>
-        <button>Login</button>
-        <button>About</button>
-        <button>Contact</button>
-      </div>
-    );
-  }
-}
 
 export default Toolbar;
