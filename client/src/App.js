9458f3d918325b018ab74fc6a378da218bb1b220
jlsnow301 <jlsnow.301@gmail.com>
2020-05-05 20:39:12 -0700

    Merged dev into jerm

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 import styled from "@emotion/styled";
 
 import Toolbar from "./components/Toolbar";
-<<<<<<< HEAD
+import { BrowserRouter as Router } from "react-router-dom";
+import Route from "react-router-dom/Route";
+import Home from "./components/views/Home";
+import About from "./components/views/About";
+import Contact from "./components/views/Contact";
+
 import InventoryDisplay from "./components/InventoryDisplay";
 
 const Body = styled.div`
@@ -11,13 +16,6 @@ const Body = styled.div`
   display: flex;
   justify-content: center;
 `;
-=======
-import { BrowserRouter as Router } from "react-router-dom";
-import Route from "react-router-dom/Route";
-import Home from "./components/views/Home";
-import About from "./components/views/About";
-import Contact from "./components/views/Contact";
->>>>>>> 364d33a98859e191392ecda07ddb9b4c5c6e8b63
 
 {
   /* @Reminder - Readme.md has resources, use it
@@ -36,28 +34,23 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className='header'>
+        <div className="header">
           <Toolbar />
         </div>
         <Router>
           {/* Browser routing for home page */}
-          <Route path='/' exact component={Home}></Route>
+          <Route path="/" exact component={Home}></Route>
           {/* Browser routing for About page */}
-          <Route path='/about' exact component={About}></Route>
+          <Route path="/about" exact component={About}></Route>
           {/* Browser routing for Contact page */}
-          <Route path='/contact' exact component={Contact}></Route>
+          <Route path="/contact" exact component={Contact}></Route>
         </Router>
+        <Body>
+          <InventoryDisplay />
+        </Body>
       </div>
-<<<<<<< HEAD
-      <Body>
-        <InventoryDisplay />
-      </Body>
-    </div>
-  );
-=======
     );
   }
->>>>>>> 364d33a98859e191392ecda07ddb9b4c5c6e8b63
 }
 
 export default App;
