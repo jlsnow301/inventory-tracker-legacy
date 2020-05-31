80f1f8177b465579353dc4a2037cde77998bca39
selinapn <57070340+selinapn@users.noreply.github.com>
2020-05-31 11:58:54 -0700

    CONTACT FORMupdate

--- client/src/components/views/Contact.js
+++ client/src/components/views/Contact.js
@@ -7,54 +7,58 @@ const Contact = () => {
     flex-direction: row;
     justify-content: space-around;
     margin-top: 50px;
-    text-align: left;
+
     font-family: sans-serif;
   `;
   const TextBox = styled.div`
     font-family: Merriweather;
-    font-size: 16pt;
-    width: 55%;
-    margin-top: 30px;
+    font-size: 30pt;
+    margin-right: 30px
+    width: 20%;
+    align: center
+    margin-top: 40px;
   `;
 
   const SubmitForm = styled.div`
     display: flex;
     flex-direction: column;
-    margin-bottom: 5px;
+    margin-top: 60px;
+    margin-right: 90px;
+  `;
+
+  const ImgTwo = styled.div`
+    width: 55%;
+    height: 40%;
+    margin-left: 60px;
+    margin-right: 0;
   `;
 
   return (
     <Container>
       <TextBox>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur soccaecat cupidatat non proident, sunt in culpa
-          qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
-        </p>
+        <ImgTwo>
+          <img src="./question.jpg" width=" 100% " alt="Pharmaceuticals" />
+        </ImgTwo>
       </TextBox>
+
       <SubmitForm>
-        <h1>We'd love to hear from you!</h1>
+        <h1>GET IN TOUCH </h1>
         <div align="right" width="50%">
           <label>First Name: </label>
-          <input size="30" type="text" id="fname" name="firstname" />
+          <input size="40" type="text" id="fname" name="firstname" />
           <br />
           <label>Last Name: </label>
-          <input size="30" type="text" id="lname" name="lastname" />
+          <input size="40" type="text" id="lname" name="lastname" />
           <br />
           <label>Email: </label>
-          <input size="30" type="text" id="email" name="email" />
+          <input size="40" type="text" id="email" name="email" />
           <br />
+          <br />
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
           <input type="submit" value="Submit" />
         </div>
       </SubmitForm>
