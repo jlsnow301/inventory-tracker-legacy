fc7204b0b7c43bd0d6c88f84941df74d7fdb2b36
selinapn <57070340+selinapn@users.noreply.github.com>
2020-06-03 14:21:24 -0700

    fixed contact img style

--- client/src/components/views/Contact.js
+++ client/src/components/views/Contact.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
-import styled from '@emotion/styled';
-import Axios from 'axios';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import Axios from "axios";
+import Modal from "react-modal";
 
 const Contact = () => {
-  const [contactFirstName, setContactFirstName] = useState('');
-  const [contactLastName, setContactLastName] = useState('');
-  const [contactEmail, setContactEmail] = useState('');
-  const [contactMessage, setContactMessage] = useState('');
+  const [contactFirstName, setContactFirstName] = useState("");
+  const [contactLastName, setContactLastName] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+  const [contactMessage, setContactMessage] = useState("");
 
   /******/
   const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
-  const [successModalMsg, setSuccessModalMsg] = useState('');
+  const [successModalMsg, setSuccessModalMsg] = useState("");
 
   // Changes the query, which is passed to inventory display.
   const handleSubmit = (e) => {
     e.preventDefault();
-    Axios.post('http://localhost:5000/api/contact', {
+    Axios.post("http://localhost:5000/api/contact", {
       firstname: contactFirstName,
       lastname: contactLastName,
       email: contactEmail,
       message: contactMessage,
     }).then((res) => {
-      if (res.data.status === '1') {
+      if (res.data.status === "1") {
         setSuccessModalMsg(res.data.message);
         setIsSuccessModalOpen(true);
       }
     });
 
-    setContactFirstName('');
-    setContactLastName('');
-    setContactEmail('');
-    setContactMessage('');
+    setContactFirstName("");
+    setContactLastName("");
+    setContactEmail("");
+    setContactMessage("");
   };
 
   const Container = styled.div`
@@ -45,25 +45,20 @@ const Contact = () => {
   const TextBox = styled.div`
     font-family: Merriweather;
     font-size: 30pt;
-    margin-right: 30px;
     width: 20%;
     align: center;
-    margin-top: 20px;
+    margin-bottom: 50px;
   `;
 
   const SubmitForm = styled.div`
     display: flex;
     flex-direction: column;
-    margin-top: 10px;
-    margin-right: 20%;
+    margin-top: 20px;
   `;
 
   const ImgTwo = styled.div`
-    width: 55%;
-    height: 25%;
-    margin-left: 30%;
-    margin-right: 0;
     filter: grayscale(35%);
+    width: 27rem;
   `;
 
   const ContactInfo = styled.div`
@@ -82,58 +77,58 @@ const Contact = () => {
     <Container>
       <TextBox>
         <ImgTwo>
-          <img src='./question.jpg' width=' 100% ' alt='Pharmaceuticals' />
+          <img src="./question.jpg" width=" 100% " alt="Pharmaceuticals" />
         </ImgTwo>
       </TextBox>
 
       <SubmitForm>
         <form onSubmit={(e) => handleSubmit(e)}>
-          <div align='right' width='50%'>
+          <div align="right" width="50%">
             <label>First Name: </label>
             <input
-              size='40'
-              type='text'
-              id='fname'
+              size="40"
+              type="text"
+              id="fname"
               value={contactFirstName}
               onChange={(e) => setContactFirstName(e.target.value)}
             />
             <br />
             <label>Last Name: </label>
             <input
-              size='40'
-              type='text'
-              id='lname'
+              size="40"
+              type="text"
+              id="lname"
               value={contactLastName}
               onChange={(e) => setContactLastName(e.target.value)}
             />
             <br />
             <label>Email: </label>
             <input
-              size='40'
-              type='text'
-              id='email'
+              size="40"
+              type="text"
+              id="email"
               value={contactEmail}
               onChange={(e) => setContactEmail(e.target.value)}
             />
             <br />
             <br />
             <textarea
-              className='form-control'
-              id='exampleFormControlTextarea1'
-              rows='3'
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
               value={contactMessage}
               onChange={(e) => setContactMessage(e.target.value)}
             ></textarea>
-            <input type='submit' value='Submit' />
+            <input type="submit" value="Submit" />
           </div>
         </form>
         <ContactInfo>
           <List>
-            <ul class='list-unstyled mb-4'>
+            <ul class="list-unstyled mb-4">
               <li>
                 <p>
                   <Icon>
-                    <ion-icon name='location-outline'></ion-icon>
+                    <ion-icon name="location-outline"></ion-icon>
                   </Icon>
                   Seattle, WA 98118, USA
                 </p>
@@ -141,14 +136,14 @@ const Contact = () => {
 
               <li>
                 <Icon>
-                  <ion-icon name='call-outline'></ion-icon>
+                  <ion-icon name="call-outline"></ion-icon>
                 </Icon>
                 <p>1-800-123-4567</p>
               </li>
 
               <li>
                 <Icon>
-                  <ion-icon name='mail-outline'></ion-icon>
+                  <ion-icon name="mail-outline"></ion-icon>
                 </Icon>
                 <p>contact@inventory.com</p>
               </li>
