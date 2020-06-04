d204c233d6b2ae2c8329a9833329bbc2c053c28e
selinapn <57070340+selinapn@users.noreply.github.com>
2020-06-03 17:12:43 -0700

    about page update

--- client/src/components/views/About.js
+++ client/src/components/views/About.js
@@ -1,45 +1,120 @@
 import React from "react";
+import { Grid, Image } from "semantic-ui-react";
 import styled from "@emotion/styled";
 
-/* Use these Comments to communicate Todos or other info
- * @Reminder - Readme.md has resources, use it
- * @Todo
- *
- */
 const About = () => {
-  // Styling
   const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    margin-top: 50px;
-  `;
-  const TextBox = styled.div`
-    font-family: Merriweather;
-    font-size: 16pt;
-    width: 55%;
-    margin-bottom: 20px;
+    margin-left: 30px;
   `;
 
-  // Returns
+  const ImgBox = styled.div`
+    align: center;
+  `;
+  const ImgIcon = styled.div`
+    width: 150px;
+    margin-left: 20%;
+    margin-bottom: 15%;
+  `;
+
+  const Banner = styled.div`
+    width: 100%;
+  `;
+
   return (
     <Container>
-      <TextBox>
-        This project was created with the help of four sleepless coders! Reach
-        us at our emails:
-      </TextBox>
-      <TextBox>
-        <b>Anthony</b>: stenbergdigeronimo@gmail.com
-        <br />
-        <b>Margarita</b>: pearlmargaret2012@gmail.com
-        <br />
-        <b>Selina</b>: selinapn@outlook.com
-        <br />
-        <b>Jeremiah</b>: jlsnow.301@gmail.com
-        <br />
-      </TextBox>
+      <Grid divided="vertically">
+        <Grid.Row columns={1}>
+          <Banner>
+            <img src="./aboutBanner.jpg" width=" 100% " alt="banner" />
+          </Banner>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <h1>volutpat</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <h1>venenatis</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns={4}>
+          <Grid.Column>
+            <ImgBox>
+              <ImgIcon>
+                <img src="./human.png" width=" 100% " alt="icon" />
+              </ImgIcon>
+            </ImgBox>
+            <h1>Anthony</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <ImgBox>
+              <ImgIcon>
+                <img src="./human.png" width=" 100% " alt="icon" />
+              </ImgIcon>
+            </ImgBox>
+            <h1>Margarita</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <ImgBox>
+              <ImgIcon>
+                <img src="./human.png" width=" 100% " alt="icon" />
+              </ImgIcon>
+            </ImgBox>
+            <h1>Jeremiah</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Grid.Column>
+          <Grid.Column>
+            <ImgBox>
+              <ImgIcon>
+                <img src="./human.png" width=" 100% " alt="icon" />
+              </ImgIcon>
+            </ImgBox>
+            <h1>Selina</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Container>
   );
 };
-
 export default About;
