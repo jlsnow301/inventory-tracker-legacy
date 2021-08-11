d52b114c624cb6d4dd2df172be439ca53a0b90c0
jlsnow301 <jlsnow.301@gmail.com>
2021-08-10 22:22:41 -0700

    Simplification (For now)

---
+++ client/webpack.config.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // Set debugging source maps to be "inline" for
+  // simplicity and ease of use
+  devtool: "inline-source-map",
+
+  // The application entry point
+  entry: "./src/index.tsx",
+
+  // Where to compile the bundle
+  // By default the output directory is `dist`
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  // Supported file loaders
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  // File extensions to support resolving
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+};
