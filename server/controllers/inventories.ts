16ad48109727c1af6081aa3e8cacd416c0efbf3f
jlsnow301 <jlsnow.301@gmail.com>
2021-08-26 18:32:24 -0700

    bulletproofing

--- server/controllers/inventories.ts
+++ server/controllers/inventories.ts
@@ -1,66 +1,118 @@
 import { RouterContext } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 
 import { Inventory } from "../models/inventory.ts";
+import HttpError from "../models/http-error.ts";
 
 type RContext = RouterContext<
   Record<string | number, string | undefined>,
   Record<string, number>
 >;
 
-export async function getInventories(ctx: RContext) {
-  const inventories = await Inventory.findAll();
-  if (inventories) {
-    ctx.response.body = { inventories: inventories };
-  } else {
-    ctx.response.body = { message: "Nothing Found!" };
+/**  Get all inventories */
+export const getInventories = async (ctx: RContext) => {
+  let inventories;
+  try {
+    inventories = await Inventory.findAll();
+  } catch (_err) {
+    throw new HttpError("Could not fetch inventories!", 500);
   }
-}
 
-export async function addInventory(ctx: RContext) {
-  const data = await ctx.request.body();
-  const body = await data.value;
-  const title = body?.title;
-  const desc = body?.description;
-  const imageUrl = body?.imageUrl;
-  const inventoryUrl = body?.url;
-  const id = await Inventory.create({
-    title: title,
-    description: desc,
-    imageUrl: imageUrl,
-    url: inventoryUrl,
-  });
+  if (!inventories) {
+    throw new HttpError("Could not find inventories!", 404);
+  }
+
+  ctx.response.body = { inventories: inventories };
+};
+
+/** Add a new inventory */
+export const addInventory = async (ctx: RContext) => {
+  let data, body, title, desc;
+  try {
+    data = await ctx.request.body();
+    body = await data.value;
+    title = body.title;
+    desc = body.description;
+  } catch (_err) {
+    throw new HttpError("Unknown Error!", 500);
+  }
+
+  if (!title || !desc) {
+    throw new HttpError("Invalid Inputs!", 430);
+  }
+
+  let id;
+  try {
+    id = await Inventory.create({
+      title: title,
+      description: desc,
+    });
+  } catch (_err) {
+    throw new HttpError("Could not create inventory!", 500);
+  }
+
   ctx.response.body = { insertedInventory: id };
-}
+};
 
-export async function updateInventory(ctx: RContext) {
-  const data = await ctx.request.body();
-  const body = await data.value;
-  const title = body?.title;
-  const desc = body?.description;
-  const imageUrl = body?.imageUrl;
-  const inventoryUrl = body?.url;
-  const id = ctx.params.inventoryId!;
+/** Update an inventory */
+export const updateInventory = async (ctx: RContext) => {
+  let id;
+  try {
+    id = ctx.params.inventoryId;
+  } catch (_err) {
+    throw new HttpError("Could not fetch inventory!", 500);
+  }
 
-  Inventory.update(id, {
-    title: title,
-    description: desc,
-    imageUrl: imageUrl,
-    url: inventoryUrl,
-  });
+  if (!id) {
+    throw new HttpError("No inventory input!", 403);
+  }
+
+  let data, body, title, desc;
+  try {
+    data = await ctx.request.body();
+    body = await data.value;
+    title = body.title;
+    desc = body.description;
+  } catch (_err) {
+    throw new HttpError("Unknown Error!", 500);
+  }
+
+  try {
+    Inventory.update(id, {
+      title: title,
+      description: desc,
+    });
+  } catch (_err) {
+    throw new HttpError("Could not update inventory!", 500);
+  }
+
   ctx.response.body = {
     message: "Updated inventory!",
     updatedInventory: {
-      title: title,
-      description: desc,
-      imageUrl: imageUrl,
-      url: inventoryUrl,
-      id: id,
+      title,
+      desc,
+      id,
     },
   };
-}
+};
 
-export async function deleteInventory(ctx: RContext) {
-  const id = ctx.params.inventoryId!;
-  await Inventory.delete(id);
+/** Delete an inventory */
+export const deleteInventory = async (ctx: RContext) => {
+  let id;
+  try {
+    id = ctx.params.inventoryId;
+  } catch (_err) {
+    throw new HttpError("Could not fetch ID!", 500);
+  }
+
+  if (!id) {
+    throw new HttpError("Could not fetch ID!", 500);
+  }
+
+  try {
+    await Inventory.delete(id);
+  } catch (_err) {
+    throw new HttpError("Could not delete inventory!", 500);
+  }
+
   ctx.response.body = { message: "Deleted inventory!" };
-}
+};
--- server/models/inventory.ts
+++ server/models/inventory.ts
@@ -1,33 +1,54 @@
 import { Bson } from "https://deno.land/x/mongo@v0.25.0/mod.ts";
 
 import getDatabase from "../helpers/db.ts";
+import HttpError from "./http-error.ts";
 
 interface Data {
   title: string;
   description: string;
-  imageUrl: string;
-  url: string;
 }
 
 export class Inventory {
   static async create(data: Data) {
-    const id = await getDatabase().collection("inventories").insertOne(data);
+    let id: Bson.Document;
+    try {
+      id = await getDatabase().collection("inventories").insertOne(data);
+    } catch (_err) {
+      throw new HttpError("Failed to create inventory!", 500);
+    }
+
+    if (!id) {
+      throw new HttpError("Could not create inventory!", 500);
+    }
+
     return { id: id.$oid };
   }
 
   static async findAll() {
-    return await getDatabase().collection("inventories").find();
+    try {
+      return await getDatabase().collection("inventories").find();
+    } catch (_err) {
+      throw new HttpError("Failed to find inventories!", 500);
+    }
   }
 
   static async update(id: string, data: Data) {
-    await getDatabase()
-      .collection("inventories")
-      .updateOne({ _id: new Bson.ObjectId(id) }, { $set: data });
+    try {
+      await getDatabase()
+        .collection("inventories")
+        .updateOne({ _id: new Bson.ObjectId(id) }, { $set: data });
+    } catch (_err) {
+      throw new HttpError("Could not update!", 500);
+    }
   }
 
   static async delete(id: string) {
-    await getDatabase()
-      .collection("inventories")
-      .deleteOne({ _id: new Bson.ObjectId(id) });
+    try {
+      await getDatabase()
+        .collection("inventories")
+        .deleteOne({ _id: new Bson.ObjectId(id) });
+    } catch (_err) {
+      throw new HttpError("Could not delete inventory!", 500);
+    }
   }
 }
