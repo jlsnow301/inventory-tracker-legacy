43630c8dfa1e0f5b448e7052d372ab5a4523835d
jlsnow301 <jlsnow.301@gmail.com>
2021-08-30 20:31:22 -0700

    Moved error

--- server/server.ts
+++ server/server.ts
@@ -6,7 +6,6 @@ import {
 
 import { connect } from "./helpers/db.ts";
 import fileRoutes from "./routes/files.ts";
-import HttpError from "./models/http-error.ts";
 import inventoryRoutes from "./routes/inventories.ts";
 import itemRoutes from "./routes/items.ts";
 import userRoutes from "./routes/users.ts";
@@ -14,6 +13,29 @@ import userRoutes from "./routes/users.ts";
 connect();
 const app = new Application();
 
+/** Error handling */
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (isHttpError(err)) {
+      switch (err.status) {
+        case Status.NotFound:
+          ctx.response.status = 404;
+          ctx.response.body = err.message || "Not found!";
+          break;
+        default:
+          ctx.response.status = err.status || 500;
+          ctx.response.body = err.message || "Unknown Error Occured";
+      }
+    } else {
+      // rethrow if you can't handle the error
+      throw err;
+    }
+  }
+});
+
+/** File handling */
 app.use(fileRoutes.routes());
 app.use(fileRoutes.allowedMethods());
 
@@ -35,28 +57,6 @@ app.use(itemRoutes.routes());
 app.use(itemRoutes.allowedMethods());
 app.use(userRoutes.routes());
 app.use(userRoutes.allowedMethods());
-
-/** Error handling */
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    if (isHttpError(err)) {
-      switch (err.status) {
-        case Status.NotFound:
-          ctx.response.status = 404;
-          ctx.response.body = "Not found!";
-          break;
-        default:
-          ctx.response.status = err.status || 500;
-          ctx.response.body = err.message || "Unknown Error Occured";
-      }
-    } else {
-      // rethrow if you can't handle the error
-      throw err;
-    }
-  }
-});
 
 app.addEventListener("error", (evt) => {
   // Will log the thrown error to the console.
